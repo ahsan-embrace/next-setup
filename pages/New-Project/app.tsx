416f87a541c262a526ab8e97db857654ed3d7038
ahsan-embrace <araz@technologies.dk>
2022-12-20 18:35:58 +0500

    my first commit

---
+++ pages/New-Project/app.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Wrapper } from "../New-Project/styles-components";
+
+const MyApp = () => {
+  return (
+    <>
+      <Wrapper>Hello World</Wrapper>
+    </>
+  );
+};
+export default MyApp;
